9cc0af3a445ea97929906dc58e1e28362b79d678
Yurii <yerikstar@gmail.com>
2025-08-29 20:17:09 +0300

    make artists-api

---
+++ src/js/artists-section.js
@@ -0,0 +1,10 @@
+import { getArtists } from './artists-api.js';
+
+async function init() {
+  try {
+    const data = await getArtists(1);
+    console.log(data);
+  } catch (err) {
+    console.error("Помилка при запиті:", err);
+  }
+}
