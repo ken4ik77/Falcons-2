ad78f3b30f7bc5ccfb67037133b74fac70a6ecb9
ken4ik77 <ken4ik77@gmail.com>
2025-09-02 22:50:26 +0000

    Deploying to gh-pages from @ ken4ik77/Falcons-2@7244c828c81ce33e17b3141de465ed2db7828282 🚀

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ import{a as o,S as m,N as g,P as v}from"./assets/vendor-DeWL0fjv.js";(function()
         `}).join("")}
       </div>
     </div>
-  `}async function u(t=1,s=8){return(await o.get("/artists",{params:{page:t,limit:s}})).data}function h(t){var s;return`
+  `}const h="/Falcons-2/assets/icons-CM5TBzXO.svg";async function u(t=1,s=8){return(await o.get("/artists",{params:{page:t,limit:s}})).data}function y(t){var s;return`
     <li class="artist-card">
       <img 
         class="artist-photo" 
@@ -74,11 +74,11 @@ import{a as o,S as m,N as g,P as v}from"./assets/vendor-DeWL0fjv.js";(function()
       <button class="learn-more" data-id="${t._id}">
         Learn More
         <svg class="icon-artists-learn-more" width="8" height="24">
-          <use xlink:href="./img/icons.svg#icon-artist-svg-1"></use>
+          <use xlink:href="${h}#icon-artist-svg-1"></use>
         </svg>
       </button>
     </li>
-  `}async function p(t,s,i){const n=document.querySelector("[data-artists-grid]"),e=document.querySelector("[data-artists-load]"),a=document.querySelector("[data-artists-loader]");a.hidden=!1,e.hidden=!0,n.insertAdjacentHTML("beforeend",t.artists.map(h).join("")),a.hidden=!0,s*i<t.totalArtists&&(e.hidden=!1),e==null||e.addEventListener("click",async()=>{s++;const r=await u(s);await p(r,s,i),d()})}(()=>{const t={openModalBtn:document.querySelector("[data-menu-open]"),closeModalBtn:document.querySelector("[data-menu-close]"),modal:document.querySelector("[data-menu]")};t.openModalBtn.addEventListener("click",s),t.closeModalBtn.addEventListener("click",s);function s(){t.modal.classList.toggle("is-open")}})();const y=`<svg style="position: absolute; width: 0; height: 0; overflow: hidden;" version="1.1" xmlns="http://www.w3.org/2000/svg"
+  `}async function p(t,s,i){const n=document.querySelector("[data-artists-grid]"),e=document.querySelector("[data-artists-load]"),a=document.querySelector("[data-artists-loader]");a.hidden=!1,e.hidden=!0,n.insertAdjacentHTML("beforeend",t.artists.map(y).join("")),a.hidden=!0,s*i<t.totalArtists&&(e.hidden=!1),e==null||e.addEventListener("click",async()=>{s++;const r=await u(s);await p(r,s,i),d()})}(()=>{const t={openModalBtn:document.querySelector("[data-menu-open]"),closeModalBtn:document.querySelector("[data-menu-close]"),modal:document.querySelector("[data-menu]")};t.openModalBtn.addEventListener("click",s),t.closeModalBtn.addEventListener("click",s);function s(){t.modal.classList.toggle("is-open")}})();const b=`<svg style="position: absolute; width: 0; height: 0; overflow: hidden;" version="1.1" xmlns="http://www.w3.org/2000/svg"
      xmlns:xlink="http://www.w3.org/1999/xlink">
     <defs>
         <symbol id="star-empty" viewBox="0 0 34 32">
@@ -97,22 +97,22 @@ import{a as o,S as m,N as g,P as v}from"./assets/vendor-DeWL0fjv.js";(function()
                   d="M16.941 25.621l10.179 6.144-2.701-11.579 8.993-7.791-11.842-1.005-4.628-10.92-4.628 10.92-11.842 1.005 8.993 7.791-2.701 11.579z"/>
         </symbol>
     </defs>
-</svg>`;async function b(){return(await o.get("/feedbacks",{params:{limit:10,page:1}})).data}async function w(t){const s=document.querySelector("div.swiper-wrapper"),i=t.map(({name:e,rating:a,descr:r},l)=>`<div class="swiper-slide">
+</svg>`;async function w(){return(await o.get("/feedbacks",{params:{limit:10,page:1}})).data}async function $(t){const s=document.querySelector("div.swiper-wrapper"),i=t.map(({name:e,rating:a,descr:r},l)=>`<div class="swiper-slide">
             <div class="feedback-card">
-                ${S(a)}
+                ${M(a)}
                 <p class="review">"${r}"</p>
                 <p class="review-author-name">${e}</p>
             </div>
-        </div>`).join("");s.insertAdjacentHTML("beforeend",i),$(t.length);const n=new m(".swiper",{modules:[g,v],direction:"horizontal",loop:!0,pagination:{el:".swiper-pagination",type:"custom",renderCustom:function(e,a,r){return L()}},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},on:{slideChange:function(){c(this.realIndex+1,t.length)}}});document.querySelector(".swiper-button-next").addEventListener("click",()=>n.slideNext()),document.querySelector(".swiper-button-prev").addEventListener("click",()=>n.slidePrev()),c(1,t.length)}function $(t){const s=document.querySelector(".swiper-pagination");s.innerHTML=`
+        </div>`).join("");s.insertAdjacentHTML("beforeend",i),L(t.length);const n=new m(".swiper",{modules:[g,v],direction:"horizontal",loop:!0,pagination:{el:".swiper-pagination",type:"custom",renderCustom:function(e,a,r){return S()}},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},on:{slideChange:function(){c(this.realIndex+1,t.length)}}});document.querySelector(".swiper-button-next").addEventListener("click",()=>n.slideNext()),document.querySelector(".swiper-button-prev").addEventListener("click",()=>n.slidePrev()),c(1,t.length)}function L(t){const s=document.querySelector(".swiper-pagination");s.innerHTML=`
         <span class="swiper-pagination-bullet custom-bullet" data-position="first" aria-label="First slide"></span>
         <span class="swiper-pagination-bullet custom-bullet" data-position="middle" aria-label="Middle slides"></span>
-        <span class="swiper-pagination-bullet custom-bullet" data-position="last" aria-label="Last slide"></span>`}function L(t,s){return`
+        <span class="swiper-pagination-bullet custom-bullet" data-position="last" aria-label="Last slide"></span>`}function S(t,s){return`
         <span class="swiper-pagination-bullet custom-bullet" data-position="first"></span>
         <span class="swiper-pagination-bullet custom-bullet" data-position="middle"></span>
-        <span class="swiper-pagination-bullet custom-bullet" data-position="last"></span>`}function c(t,s){const i=document.querySelectorAll(".custom-bullet");i.forEach(n=>n.classList.remove("swiper-pagination-bullet-active")),t===1?i[0].classList.add("swiper-pagination-bullet-active"):t===s?i[2].classList.add("swiper-pagination-bullet-active"):i[1].classList.add("swiper-pagination-bullet-active")}function S(t){const s="star-rating-sprite";if(!document.getElementById(s)){const e=document.createElement("div");e.id=s,e.style.position="absolute",e.style.width="0",e.style.height="0",e.style.overflow="hidden",e.innerHTML=y,document.body.prepend(e)}const i=Math.round(t),n=Array.from({length:5},(e,a)=>`
+        <span class="swiper-pagination-bullet custom-bullet" data-position="last"></span>`}function c(t,s){const i=document.querySelectorAll(".custom-bullet");i.forEach(n=>n.classList.remove("swiper-pagination-bullet-active")),t===1?i[0].classList.add("swiper-pagination-bullet-active"):t===s?i[2].classList.add("swiper-pagination-bullet-active"):i[1].classList.add("swiper-pagination-bullet-active")}function M(t){const s="star-rating-sprite";if(!document.getElementById(s)){const e=document.createElement("div");e.id=s,e.style.position="absolute",e.style.width="0",e.style.height="0",e.style.overflow="hidden",e.innerHTML=b,document.body.prepend(e)}const i=Math.round(t),n=Array.from({length:5},(e,a)=>`
             <svg class="star ${a<i?"filled":"empty"}" aria-hidden="true" width="20" height="20">
                 <use href="#star-filled"></use>
             </svg>`).join("");return`<div class="rating star-svg value-${i}" aria-label="Rating ${i} out of 5">
         <div class="star-container">${n}</div>
-        </div>`}o.defaults.baseURL="https://sound-wave.b.goit.study/api";async function M(){const i=await u(1);console.log(`Artists: ${JSON.stringify(i)}`),await p(i,1,8),d();const n=await b();console.log(`Review: ${JSON.stringify(n)}`),await w(n.data)}M();
+        </div>`}o.defaults.baseURL="https://sound-wave.b.goit.study/api";async function A(){const i=await u(1);console.log(`Artists: ${JSON.stringify(i)}`),await p(i,1,8),d();const n=await w();console.log(`Review: ${JSON.stringify(n)}`),await $(n.data)}A();
 //# sourceMappingURL=index.js.map
