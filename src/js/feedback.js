3d3430394c8a44545e3de4e5c53ae207df3a62df
Stanislav Hanchev <stanislav@smoothr.de>
2025-09-02 05:20:44 +0300

    fix arrow icons somehow*

--- src/js/feedback.js
+++ src/js/feedback.js
@@ -11,6 +11,7 @@ import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'css-star-rating/css/star-rating.min.css';
 import starSprite from '../img/star-rating.icons.svg?raw';
+import mainSprite from '../img/icons.svg?raw';
 
 export async function getReviewsByQuery() {
     const response = await axios.get('/feedbacks', {
@@ -51,6 +52,18 @@ export async function handleReviews(reviews) {
         }
     });
 
+    // const ICON_SPRITE_MOUNT_ID = 'main-icon-sprite';
+    // if (!document.getElementById(ICON_SPRITE_MOUNT_ID)) {
+    //     const holder = document.createElement('div');
+    //     holder.id = ICON_SPRITE_MOUNT_ID;
+    //     holder.style.position = 'absolute';
+    //     holder.style.width = '0';
+    //     holder.style.height = '0';
+    //     holder.style.overflow = 'hidden';
+    //     holder.innerHTML = mainSprite;
+    //     document.body.prepend(holder);
+    // }
+
     document.querySelector('.swiper-button-next').addEventListener('click', () => swiper.slideNext());
     document.querySelector('.swiper-button-prev').addEventListener('click', () => swiper.slidePrev());
 }
@@ -58,14 +71,14 @@ export async function handleReviews(reviews) {
 function setRating(rating) {
     const SPRITE_MOUNT_ID = 'star-rating-sprite';
     if (!document.getElementById(SPRITE_MOUNT_ID)) {
-        const holder = document.createElement('div');
-        holder.id = SPRITE_MOUNT_ID;
-        holder.style.position = 'absolute';
-        holder.style.width = '0';
-        holder.style.height = '0';
-        holder.style.overflow = 'hidden';
-        holder.innerHTML = starSprite;
-        document.body.prepend(holder);
+        const div = document.createElement('div');
+        div.id = SPRITE_MOUNT_ID;
+        div.style.position = 'absolute';
+        div.style.width = '0';
+        div.style.height = '0';
+        div.style.overflow = 'hidden';
+        div.innerHTML = starSprite;
+        document.body.prepend(div);
     }
     const rounded = Math.round(rating);
     const stars = Array.from({ length: 5 }, (_, i) => {
