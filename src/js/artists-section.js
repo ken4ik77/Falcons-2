fdba868474cedef4759e7e586d4c7c47ac164c55
Ken4ik77 <ken4ik77@gmail.com>
2025-09-03 01:22:20 +0300

    Update artists-section.js

--- src/js/artists-section.js
+++ src/js/artists-section.js
@@ -35,7 +35,7 @@ function createArtistCard(artist) {
       <button class="learn-more" data-id="${artist._id}">
         Learn More
         <svg class="icon-artists-learn-more" width="8" height="24">
-          <use href="./img/icons.svg#icon-learn-more"></use>
+          <use href="./img/icons.svg#icon-artist-svg-1"></use>
         </svg>
       </button>
     </li>
