f4894ea214b6194eca4706cb4cdcd632bf5d6fb4
ken4ik77 <ken4ik77@gmail.com>
2025-09-02 22:23:26 +0000

    Deploying to gh-pages from @ ken4ik77/Falcons-2@bbad528e87fec80070fa4b2fa9dd91d817796da5 🚀

--- index.js
+++ index.js
@@ -74,7 +74,7 @@ import{a as o,S as m,N as g,P as v}from"./assets/vendor-DeWL0fjv.js";(function()
       <button class="learn-more" data-id="${t._id}">
         Learn More
         <svg class="icon-artists-learn-more" width="8" height="24">
-          <use href="./img/icons.svg#icon-learn-more"></use>
+          <use href="./img/icons.svg#icon-artist-svg-1"></use>
         </svg>
       </button>
     </li>
